a4c71d032a9cbb7866113466d6d10bb1bc74a051
gon tana <gontana1220@gmail.com>
2021-03-10 14:01:59 +0900

    useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(count * 2)
+  const div3 = () => {
+    if (count % 3 == 0) {
+      return setCount(count / 3)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+      </div>
+      <div>
+        <button onClick={double}>x2</button>
+      </div>
+      <div>
+        <button onClick={div3}>÷3</button>
+      </div>
+
+    </>
   );
 }
 
